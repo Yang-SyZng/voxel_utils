1155be19aa4f6c8dee925f54cb29b2ef0c7b3c9c
Gardenia. <73323762+Yang-SyZng@users.noreply.github.com>
2025-12-19 09:38:57 +0800

    Delete archive directory

--- archive/package/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# utils/__init__.py
-from typing import Final
-import torch
-
-DOUBLE: Final = torch.double
-FLOAT64: Final = torch.float64
-FLOAT32: Final = torch.float32
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
